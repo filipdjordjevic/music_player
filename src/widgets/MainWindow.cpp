b1f04c6ecb330c5d4a549f3239dd7b55892177f2
filip <filip270798@gmail.com>
2020-09-08 19:58:44 +0200

    Add custom titlebar

--- src/widgets/MainWindow.cpp
+++ src/widgets/MainWindow.cpp
@@ -2,15 +2,68 @@
 
 namespace ui
 {
-    MainWindow::MainWindow(QWidget *parent) : QWidget(parent)
+    MainWindow::MainWindow(QWidget *parent)
+        : QWidget(parent)
     {
+        this->initTitlebar();
+
         player_ = new PlayerFrame;
 
         QVBoxLayout *mainLayout = new QVBoxLayout;
-
+        mainLayout->setContentsMargins(0, 0, 0, 0);
+        mainLayout->addLayout(titleBtns_);
         mainLayout->addWidget(player_);
 
         setLayout(mainLayout);
         this->layout()->setSizeConstraint(QLayout::SetFixedSize);
+    }
+
+    void MainWindow::paintEvent(QPaintEvent *)
+    {
+        QPainter painter(this);
+        QColor bg = QColor("#242226");
+        painter.fillRect(QRect(0, 0, this->width(), 20), bg);
+    }
+
+    void MainWindow::mousePressEvent(QMouseEvent *event)
+    {
+        if (event->button() == Qt::LeftButton)
+        {
+            dragPosition_ = event->globalPos() - frameGeometry().topLeft();
+            event->accept();
+        }
+    }
+
+    void MainWindow::mouseMoveEvent(QMouseEvent *event)
+    {
+        if (event->buttons() & Qt::LeftButton)
+        {
+            move(event->globalPos() - dragPosition_);
+            event->accept();
+        }
+    }
+
+    void MainWindow::initTitlebar()
+    {
+        this->setWindowFlag(Qt::FramelessWindowHint);
+
+        titleBtns_ = new QHBoxLayout;
+
+        QPushButton *openBtn = new QPushButton("Open");
+        openBtn->setObjectName("titlebar-open");
+        QObject::connect(openBtn, &QPushButton::clicked, this, [this] { QFileDialog::getOpenFileUrl(this, "Open mp3 file", QDir::homePath()); });
+
+        QPushButton *closeBtn = new QPushButton("X");
+        closeBtn->setObjectName("titlebar");
+        QObject::connect(closeBtn, &QPushButton::clicked, this, [this] { this->close(); });
+
+        QPushButton *minimizeBtn = new QPushButton("-");
+        minimizeBtn->setObjectName("titlebar");
+        QObject::connect(minimizeBtn, &QPushButton::clicked, this, [this] { this->setWindowState(Qt::WindowMinimized); });
+
+        titleBtns_->addWidget(openBtn);
+        titleBtns_->addStretch();
+        titleBtns_->addWidget(minimizeBtn);
+        titleBtns_->addWidget(closeBtn);
     }
 } // namespace ui
--- src/widgets/MainWindow.hpp
+++ src/widgets/MainWindow.hpp
@@ -12,8 +12,16 @@ namespace ui
 
     private:
         PlayerFrame *player_;
+        QHBoxLayout *titleBtns_;
+        QPoint dragPosition_;
 
     public:
         MainWindow(QWidget *parent = NULL);
+        void initTitlebar();
+
+    protected:
+        void mouseMoveEvent(QMouseEvent *event) override;
+        void mousePressEvent(QMouseEvent *event) override;
+        void paintEvent(QPaintEvent *event) override;
     };
 } // namespace ui
